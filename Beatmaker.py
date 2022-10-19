67ef50d51ce366fb314e0c4d030592699d69aa22
Surya <Surya.nara0123@gmail.com>
2022-10-19 20:41:29 +0530

    created buttons

--- Beatmaker.py
+++ Beatmaker.py
@@ -11,6 +11,14 @@ class BeatSequencer:
         pyg.init()
         window = pyg.display.set_mode((800, 600))
         run = True
+        flagList = [
+            [False, False, False, False, False, False],
+            [False, False, False, False, False, False],
+            [False, False, False, False, False, False],
+            [False, False, False, False, False, False],
+            [False, False, False, False, False, False],
+            [False, False, False, False, False, False]
+        ]
         rectList = [
             [],
             [],
@@ -20,17 +28,28 @@ class BeatSequencer:
             []
         ]
         for j, list in enumerate(rectList):
-            for i in range(10):
-                list.append(pyg.Rect((window.get_width()-100)/6*(i+1)-15, (window.get_height()/6)*j,(window.get_width()-100)/6, (window.get_height()/6)))
+            for i in range(6):
+                list.append(pyg.Rect((window.get_width()-100)/6*(i+1)-10, (window.get_height()/6)*j+5,(window.get_width()-100)/6-5, (window.get_height()/6)-5))
         while run:
             window.fill((0,0,0))
             pyg.draw.rect(window, (80, 80, 80), (0, 0, 100, window.get_height()))
             for i in rectList:
                 for j in i:
-                    pyg.draw.rect(window, (80, 80, 80), j, 4)
+                    if flagList[rectList.index(i)][i.index(j)] == False:
+                        pyg.draw.rect(window, (80, 80, 80), j, 0, 10)
+                    else:
+                        pyg.draw.rect(window, (80, 180, 80), j, 0, 10)
             for event in pyg.event.get():
                 if event.type == pyg.QUIT:
                     run = False
+                if event.type == pyg.MOUSEBUTTONDOWN:
+                    for i in rectList:
+                        for j in i:
+                            if j.collidepoint(pyg.mouse.get_pos()):
+                                if flagList[rectList.index(i)][i.index(j)] == False:
+                                    flagList[rectList.index(i)][i.index(j)] = True
+                                else:
+                                    flagList[rectList.index(i)][i.index(j)] = False
 
             pyg.display.update()
     def menu(self):
