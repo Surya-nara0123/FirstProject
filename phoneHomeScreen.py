07a6219471a5a0ca52976856fccd0ac85448fa49
Surya <Surya.nara0123@gmail.com>
2022-11-02 09:47:29 +0530

    weird shit

--- phoneHomeScreen.py
+++ phoneHomeScreen.py
@@ -1,17 +1,23 @@
-import pygame as pyg, sys, cv2, random
+import pygame as pyg, sys, cv2, random, os
 
 class homeScreen:
     def mainScreen(self):
-        width, height = 400, 700
+        width, height = 600, 900
         window = pyg.display.set_mode((width, height))
         images = []
-        for i in range(10):
-            images.append(pyg.image.load(f"/Users/surya/Desktop/SuryaFolder/SuryaAssets/nature/Image_{i+1}.jpg"))
+        for i in os.listdir("/Users/surya/Desktop/SuryaFolder/SuryaAssets/nature"):
+            images.append(pyg.image.load(f"/Users/surya/Desktop/SuryaFolder/SuryaAssets/nature/{i}"))
         run = True
-        image = random.choice(images)
-        image = pyg.transform.smoothscale(image, (image.width, height))
+        image = images[3]
+        #image = random.choice(images)
+        image = pyg.transform.smoothscale(image, (width, height))
+        #image = pyg.transform.chop(image, (0, image.get_height()-100, image.get_width(), 100))
         while run:
             window.blit(image, (0, 0))
+            buttons = pyg.Surface((image.get_width(), 50))
+            buttons.set_alpha(100)
+            buttons.fill((255, 255, 255))
+            window.blit(buttons, (0, image.get_height()-50))
             for event in pyg.event.get():
                 if event.type == pyg.QUIT:
                     run = False
